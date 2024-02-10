e79f961613b5ba38614c20a1ede7fe941c589406
Shubham Singh <shubhammahar1306@gmail.com>
2024-02-10 10:28:33 +0000

    app js is fine

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,47 +1,33 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.css";
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./components/Home/Home.jsx";
+import LoginPage from "./components/loginPage/LoginPage.jsx";
+import Entries from "./components/Tracker/Entries";
 import Footer from "./components/footer/Footer.jsx";
 import QuoteOne from "./components/quoteOne/QuoteOne.jsx";
 import Me from "./components/me/Me.jsx";
 import QuoteTwo from "./components/quoteTwo/QuoteTwo.jsx";
 import Navbar from "./components/navbar/Navbar.jsx";
 import Caltech from "./components/caltech/Caltech";
-import LoginPage from "./components/loginPage/LoginPage.jsx";
-
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import BMI from "./components/BMI/BMI.jsx";
 
 function App() {
   return (
     <>
-    <Router>
       <div className="all">
+        <BrowserRouter>
+          <Routes>
+            <Route path="/login" element={<LoginPage />} />
 
-        <Routes>
-          <Route path="/login" element={
-                  <LoginPage />          
-          } />
-          <Route
-            path="/"
-            element={
-              <>
-        <Navbar />
-        <Caltech />
-        <QuoteOne />
-        
-        <Entries />
-
-        <QuoteTwo />
-        <Me />
-              </>
-            }
-          />
-        </Routes>
-
-
-      <Footer />
+            <Route path="/" element={<Home />}>
+              <Route index element={<Home />} />
+              {/* <Route path="blogs" element={<Blogs />} />
+              <Route path="contact" element={<Contact />} /> */}
+            </Route>
+          </Routes>
+        </BrowserRouter>
       </div>
-      </Router>
     </>
   );
 }
