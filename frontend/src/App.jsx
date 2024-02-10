f4e47a442ce18ec149e2b606eabd6dfa130d66ba
Shubham Singh <shubhammahar1306@gmail.com>
2024-02-10 06:21:15 +0000

    navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react'
 import {Button} from "./components/ui/button"
 import {Procarousel} from "./components/slideShow/carousel"
+import {Navbar} from "./components/navbar/navbar"
 import './App.css'
 
 function App(){
   return (
     <>
+    <Navbar />
     <Procarousel />
     <Button>Log In!</Button>
     </>
