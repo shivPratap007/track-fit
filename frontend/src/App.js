e7813ee2ad195f3c3f182c569916a66bff45c3bf
Shiv Pratap <sp09singhwaghel@gmail.com>
2024-02-10 15:51:07 +0530

    add : react router feature

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,8 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.css";
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./components/Home/Home.jsx";
+import LoginPage from "./components/loginPage/LoginPage.jsx";
 import Entries from "./components/Tracker/Entries";
 import Footer from "./components/footer/Footer.jsx";
 import QuoteOne from "./components/quoteOne/QuoteOne.jsx";
@@ -10,19 +12,21 @@ import Navbar from "./components/navbar/Navbar.jsx";
 import Caltech from "./components/caltech/Caltech";
 import BMI from "./components/BMI/BMI.jsx";
 
-
 function App() {
   return (
     <>
       <div className="all">
-        <Navbar />
-        <Caltech />
-        <QuoteOne />
-        <BMI/>
-        <Me />
-        {/* <QuoteTwo /> */}
-        <Entries />
-        <Footer />
+        <BrowserRouter>
+          <Routes>
+            <Route path="/login" element={<LoginPage />} />
+
+            <Route path="/" element={<Home />}>
+              <Route index element={<Home />} />
+              {/* <Route path="blogs" element={<Blogs />} />
+              <Route path="contact" element={<Contact />} /> */}
+            </Route>
+          </Routes>
+        </BrowserRouter>
       </div>
     </>
   );
--- frontend/src/components/Home/Home.jsx
+++ frontend/src/components/Home/Home.jsx
@@ -1,11 +1,19 @@
 import React from 'react'
 import Navbar from '../navbar/Navbar'
+import Caltech from '../caltech/Caltech'
+import QuoteOne from '../quoteOne/QuoteOne';
+import Me from '../me/Me';
+import Footer from '../footer/Footer';
 
 const Home = () => {
   return (
     <>
     <Navbar/>
-    
+    <Caltech/>
+    <QuoteOne />
+    <Me />
+    <Footer />
+
     </>
   )
 }
