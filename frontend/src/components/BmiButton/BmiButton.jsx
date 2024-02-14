7e63a3e211826a7e4b243ff40c1b04c4539a8d45
Shubham Singh <shubhammahar1306@gmail.com>
2024-02-14 07:09:48 +0000

    styling the bmi part

--- frontend/src/components/BmiButton/BmiButton.jsx
+++ frontend/src/components/BmiButton/BmiButton.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import "./button.css"
 
 const BmiButton = () => {
   return (
-    <button>
+    <button className='bbutton'>
         <Link to="/bmi">Calculate BMI</Link>
     </button>
   )
